9b923cc71e810afa34b8b9b32d3661a4006c2f4b
Synerg'hetic <dsi@synerghetic.net>
2021-09-09 13:07:46 +0200

    Initial Commit

---
+++ Shared/TempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempoApp.swift
+//  Shared
+//
+//  Created by Romain Penchenat on 09/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct TempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
